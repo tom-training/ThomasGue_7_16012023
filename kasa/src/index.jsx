a52875adc8fd6c5ef0b81d2746d39eb3112ab429
tom-training <thomasgue77@gmail.com>
2023-01-16 22:18:03 +0100

    premiere structure avec Router

---
+++ kasa/src/index.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <Router>
+
+      <Routes>
+
+        <Route exact path="/" element={<Home />} />
+
+      </Routes>
+    
+        
+
+
+    </Router>
+    
+  </React.StrictMode>
+);
+
+
+
---
+++ kasa/src/pages/Home/index.jsx
@@ -0,0 +1,9 @@
+function Home() {
+    return (
+      <div>
+        C'est le composant HOME ! de l'appli Kasa
+      </div>
+    );
+  }
+  
+  export default Home;
